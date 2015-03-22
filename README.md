214fb5903a7b9c4f70ef0c15bb3cea6bb84e2909
Javier Lopez <javierlopm@hotmail.com>
2015-03-22 17:01:24 -0430

    Moved from tweepy to twython

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tweetapps
 Twitter miscellaneous applications
 
-Python applications using tweepy library (https://github.com/tweepy/tweepy).
+Python applications using twython library (https://github.com/ryanmcgrath/twython).
 
 Some of this apps are meant to only be used on a Raspberry Pi (temperature check).
--- checkMyTemp.py
+++ checkMyTemp.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # RaspberryPi app
 # 
 # Developed by Javier Lopez
 # 03/22/2015
 
-import tweepy
+from twython import Twython
 import sys
 import re
 import datetime
@@ -17,35 +17,30 @@ import subprocess
 status = subprocess.check_output(["/opt/vc/bin/vcgencmd","measure_temp"])
 m      = re.search('[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?',status)
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 temp = float(m.group(0))
 
 #Messages according to temp, if less than 40, exit and don't tweet about it
-if( 40 <= temp and temp < 50):
+if  (40 <= temp and temp<50):
     string = 'Hey...'
 elif(50 <= temp and temp<60):
     string = 'Subiendo'
 elif(60 <= temp):
     string = 'Dude @javierlopm hace calor aqui'
 else:
-    string = " "
-    #exit()
+    exit()
 
 
 #Reading keys under the password file 
 keys = open('password','r')
-consumerKey         = keys.readline()
-consumerSecret      = keys.readline()
-acessToken          = keys.readline()
-acessTokenSecret    = keys.readline()
+consumerKey         = keys.readline()[:-1]
+consumerSecret      = keys.readline()[:-1]
+acessToken          = keys.readline()[:-1]
+acessTokenSecret    = keys.readline()[:-1]
 keys.close()
 
-#Tweepy init
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(acessToken, acessTokenSecret)
-api = tweepy.API(auth)
+#Twython init
+twitter = Twython(consumerKey, consumerSecret,acessToken, acessTokenSecret)
+twitter.verify_credentials()
 
 #Tweet temperature, according to the hour (Spanish hour word pick)
 if(string,datetime.datetime.now().strftime('%I') == '01' ):
@@ -61,4 +56,4 @@ else:
                                     float(m.group(0))
                                                   )
 
-status = api.update_status(string)
+status = twitter.update_status(status=string)
