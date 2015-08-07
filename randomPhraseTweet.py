0c9a58b8709f32cbc1f8b644d85f5db04331727d
Javier Lopez <javierlopm@hotmail.com>
2015-08-07 13:27:33 -0430

    Phrase generator

---
+++ randomPhraseTweet.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# RaspberryPi app
+# 
+# Developed by Javier Lopez
+# 03/22/2015
+
+import tweepy
+import sys
+import re
+import datetime
+import subprocess
+
+
+
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+
+
+#Reading keys under the password file 
+keys = open('password','r')
+consumerKey         = keys.readline()[:-1]
+consumerSecret      = keys.readline()[:-1]
+acessToken          = keys.readline()[:-1]
+acessTokenSecret    = keys.readline()[:-1]
+keys.close()
+
+#Tweepy init
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(acessToken, acessTokenSecret)
+api = tweepy.API(auth)
+
+#Tweet temperature, according to the hour (Spanish hour word pick)
+from randomPhrases.makePhrase import *
+d = dictionary()
+
+string = "The" + d.makePhraseFromCode(["A","N","t"]) + " #randomPhrase"
+
+status = api.update_status(status=string)
